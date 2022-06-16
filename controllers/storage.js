7975512a79da2a4903ab786b7ea02acf667fe162
Ttito19 <andreschavez.2000@gmail.com>
2022-06-16 15:26:45 -0500

    changes data

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,30 +1,30 @@
-const {storageModel}=require("../models");
+const { storageModel } = require("../models");
 /**
  * Obtener lista 
  * @param {*} req 
  * @param {*} res 
  */
-const getItems= async (req,res)=>{
+const getItems = async (req, res) => {
 
-    const data= await storageModel.find({})
-    res.send({data})
+    const data = await storageModel.find({})
+    res.send({ data })
 }
 /**
  * Obtener un detalle 
  * @param {*} req 
  * @param {*} res 
  */
-const getItem=(req,res)=>{}
+const getItem = (req, res) => { }
 /**
  * Insertar un registro 
  * @param {*} req 
  * @param {*} res 
  */
-const createItem= async (req,res)=>{
-const {body}=req;
-console.log(body);
-const data= await storageModel.create(body)
-res.send({data} )
+const createItem = async (req, res) => {
+    const { body, file } = req;
+    console.log(file);
+    //const data= await storageModel.create(body)
+    res.send(file)
 
 }
 /**
@@ -32,14 +32,14 @@ res.send({data} )
  * @param {*} req 
  * @param {*} res 
  */
-const updateItem=(req,res)=>{}
+const updateItem = (req, res) => { }
 /**
  * Eliminar registro 
  * @param {*} req 
  * @param {*} res 
  */
-const deleteItem=(req,res)=>{}
+const deleteItem = (req, res) => { }
 
 
 
-module.exports={getItems,getItem,createItem,updateItem,deleteItem};
+module.exports = { getItems, getItem, createItem, updateItem, deleteItem };
